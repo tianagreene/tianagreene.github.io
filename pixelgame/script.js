37c2fdf4d3a5544c624b8aa4a4abb27969fdcde7
tiana-gif <76976070+tiana-gif@users.noreply.github.com>
2021-05-03 22:07:11 -0700

    lots of stuffs

--- pixelgame/script.js
+++ pixelgame/script.js
@@ -1,17 +1,17 @@
 $( "#target" ).keydown(function( event ) {
   if ( event.which == 37 || event.which == 65) {
    event.preventDefault();
-   $('.person').attr('src', 'images/running.png')
+   $('.person').attr('src', 'images/spritey.gif')
    $(".wrapper").removeClass("forward")
    //alert('left arrow!')
   } else if( event.which == 39 || event.which == 68 ) {
    event.preventDefault();
-   $('.person').attr('src', 'images/running.png')
+   $('.person').attr('src', 'images/spritey.gif')
    $(".wrapper").addClass("forward")
    //alert('right arrow!')
   } else if( event.which == 38 || event.which == 87 ) {
    event.preventDefault();
-   $('.person').attr('src', 'images/jumper.png')
+   $('.person').attr('src', 'images/spritey.gif')
    $('.person-space').css('top', '40px').delay( 800 )
    $('.person-space').css('top', '80px')
    //$('.person').css('translate', 'transformY(50px)')
